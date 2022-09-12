4ec904fae51edf28ecec7dc8c339420c0a34c81e
thahseen53 <thahseen53@gmail.com>
2022-09-12 16:36:45 +0530

    sorce path changed to placeholder.png

--- frontend/src/components/ItemPreview.js
+++ frontend/src/components/ItemPreview.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import agent from "../agent";
 import { connect } from "react-redux";
 import { ITEM_FAVORITED, ITEM_UNFAVORITED } from "../constants/actionTypes";
-import defaultSrc from '../../public/placeholder.png'
+
 
 const mapDispatchToProps = (dispatch) => ({
   favorite: (slug) =>
@@ -37,7 +37,7 @@ const ItemPreview = (props) => {
     >
       <img
         alt="item"
-        src={defaultSrc}
+        src='../../public/placeholder.png'
         className="card-img-top item-img"
         style={{ borderRadius: "20px" }}
       />
